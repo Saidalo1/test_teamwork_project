090f8735126f7034fbfbe1ce566fd9d5c2449849
Saydaloxonsuper12 <super.saydalo@mail.ru>
2023-01-07 19:59:23 +0500

    Clear code

--- root/urls.py
+++ root/urls.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib import admin
 
 from django.urls import re_path, include, path
 from drf_yasg import openapi
