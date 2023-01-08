fdf9c948d7a881bcb7e44ee0f9642eea5d2d21a9
laziztojiyev <laziztojiyev45@gmail.com>
2023-01-08 11:33:48 +0500

    alter model product description

--- orders/models.py
+++ orders/models.py
@@ -25,6 +25,6 @@ class Product(Model):
     name = CharField(max_length=255)
     price = DecimalField(max_digits=9, decimal_places=2, default=0)
     img = ImageField(upload_to='products/')
-    description = TextField(max_length=500)
+    description = TextField()
     category = ForeignKey('orders.Category', CASCADE)
     shop = ForeignKey('orders.Shop', SET_NULL, null=True, blank=True)
